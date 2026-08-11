53e84937e804f3ad415425b486998de5ac0ffb86
Apple <apple@Apples-MacBook-Air.local>
2026-08-11 05:34:32 +0530

    add airflow DAG with 4 pipeline tasks

---
+++ airflow/Telco-dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+import datetime
+from airflow.providers.standard.operators.bash import BashOperator
+
+
+
+with DAG(
+    dag_id="Telco-pipeline",
+    start_date=datetime.datetime(2020, 1, 1),
+    schedule='@daily',
+    catchup=False
+) as dag:
+
+    extractVal_task = BashOperator(
+        task_id="load-data-validate",
+        bash_command="python3 /Users/apple/Desktop/project-main/pyspark/validate_csv.py"
+    )
+
+    convert_parquet = BashOperator(
+        task_id="convert-parquet",
+        bash_command="python3 /Users/apple/Desktop/project-main/pyspark/csv_to_parquet.py"
+    )
+
+    json_contract = BashOperator(
+        task_id="json-contract",
+        bash_command="python3 /Users/apple/Desktop/project-main/pyspark/create_json_contract.py"
+    )
+    dbt_run = BashOperator(
+        task_id="dbt-run",
+        bash_command="cd /Users/apple/Desktop/project-main/telco_dbt && dbt run --exclude example"
+    )
+
+    extractVal_task >> convert_parquet >> json_contract >> dbt_run
